faa2789950ed6d8b7deed25caf79b0969558d9b4
yebooni <lovemuney4@gmail.com>
2017-10-25 19:19:37 +0900

    qna 추가
    택우꺼에 추가했는데 왜떄문에 db안되유?

---
+++ AMIGOPRJ/AmigoProject/src/main/java/com/amigo/dao/qna/QnADAO.java
@@ -0,0 +1,23 @@
+package com.amigo.dao.qna;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.amigo.vo.qna.QnADTO;
+
+
+
+public interface QnADAO {
+
+	public void insertQna(QnADTO qnaDto);
+	
+	public List<QnADTO> selectQna();
+	
+	public QnADTO viewQna(int num);
+	
+	public void deleteQna(int num);
+	
+	public void updateQna(QnADTO qnaDto);
+	
+}
